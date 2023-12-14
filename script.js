ded903f824668ca17703b72bd96df218919a4c55
javiergonzalez02 <javi.galeon2012@gmail.com>
2023-12-14 20:24:23 +0100

    Añadida funcionalidad de cambio de color

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let background = document.querySelector("body");
+let button = document.querySelector("button");
+let colors = ["#FF5733", "#33FF57", "#3357FF", "#F033FF", "#FF33F0", "#33FFF5", "#F5FF33", "#FF8C33", "#338CFF", "#8333FF"];
 
-background.style.backgroundColor = "#ff4";
+background.style.backgroundColor = "red";
+
+const cambiarColorFondo = () => {
+    background.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+}
+
+button.addEventListener("click", cambiarColorFondo);
